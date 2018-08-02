acb774df736c252679c71d32575f73fbbe4289ad
Ivan Smirnov <isgsmirnov@gmail.com>
2018-08-01 17:30:40 -0700

    Update zap to 0.9.7
    Also fixed all audit issues:
    - brew audit --strict --online zap

--- Formula/zap.rb
+++ Formula/zap.rb
@@ -1,9 +1,9 @@
 class Zap < Formula
   desc "Recursive URL expander"
   homepage "https://github.com/issmirnov/zap"
-  url "https://github.com/issmirnov/zap/releases/download/v0.9.6/zap_MacOS_64-bit.tar.gz"
-  version "0.9.6"
-  sha256 "e37a291875d84af3083c9bfcb8f2e4de86b56024109f99643017adb3bd73db9c"
+  url "https://github.com/issmirnov/zap/releases/download/v0.9.7/zap_MacOS_64-bit.tar.gz"
+  version "0.9.7"
+  sha256 "701a8224643a003163b01c649fa0baccd9b1651a062eecf0959159bc59d8cd30"
 
   bottle :unneeded
 
@@ -13,30 +13,26 @@ class Zap < Formula
     (etc/"zap").install "c.yml"
   end
 
-  test do
-    system "#{bin}/zap", "-v"
-  end
-
   def caveats
-    s = <<-EOS.undent
-    #{name} will listen on port 80 for requests. Using config file:
-    #{etc}/zap/c.yml. Zap supports hot reloading, so simply edit the
-    file and try your new shortcuts.
-    
-    If you would like to run this as a system service, simply run 
-    `sudo brew services start zap`
-    
-    If you would like to run this behind a webserver proxy, see 
-    https://github.com/issmirnov/zap/blob/master/README.md#osx-brew.
-    Be sure to update /etc/hosts with your shortcuts.
+    s = <<~EOS
+      #{name} will listen on port 80 for requests. Using config file:
+      #{etc}/zap/c.yml. Zap supports hot reloading, so simply edit the
+      file and try your new shortcuts.
 
-    NOTE: Please restart the service after upgrading!
+      If you would like to run this as a system service, run
+      `sudo brew services start zap`
+
+      If you would like to run this behind a webserver proxy, see
+      https://github.com/issmirnov/zap/blob/master/README.md#osx-brew.
+      Be sure to update /etc/hosts with your shortcuts.
+
+      NOTE: Please restart the service after upgrading!
     EOS
     s
   end
 
-  def plist;
-  <<-EOS.undent
+  plist_options :manual => "zap"
+  def plist; <<~EOS
     <?xml version="1.0" encoding="UTF-8"?>
     <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
     <plist version="1.0">
@@ -57,6 +53,10 @@ class Zap < Formula
         </array>
       </dict>
     </plist>
-    EOS
+  EOS
+  end
+
+  test do
+    system "#{bin}/zap", "-v"
   end
 end
